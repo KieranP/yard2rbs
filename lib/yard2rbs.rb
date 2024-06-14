6af26271e2664004f7530a6e1acd7ba0c75293f8
Kieran Pilkington <kieran776@gmail.com>
2024-06-14 17:50:24 +1200

    Initial Commit

---
+++ lib/yard2rbs.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "yard2rbs/version"
+
+module Yard2rbs
+  class Error < StandardError; end
+  # Your code goes here...
+
+  def self.convert(file)
+
+  end
+end
---
+++ spec/yard2rbs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Yard2rbs do
+  it "has a version number" do
+    expect(Yard2rbs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
